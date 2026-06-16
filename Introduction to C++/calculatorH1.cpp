8be748d97c032604ec8a9a4690c98f605f800314
Yrajdixit <yrajdixit@gmail.com>
2026-06-17 01:53:38 +0530

    Add files via upload

---
+++ Introduction to C++/calculatorH1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    cout << N << " + " << M << " = " << N + M << endl;
+    cout << N << " - " << M << " = " << N - M << endl;
+    cout << N << " * " << M << " = " << N * M << endl;
+    cout << N << " / " << M << " = " << N / M << endl;
+    cout << N << " % " << M << " = " << N % M << endl;
+
+    return 0;
+}
