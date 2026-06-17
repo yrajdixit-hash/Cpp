6dd13578d36910a049129370262b6fb5e30c4fc0
Yrajdixit <yrajdixit@gmail.com>
2026-06-18 00:25:55 +0530

    Add files via upload

---
+++ Introduction to C++/calculator__II.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long N, M;
+    cin >> N >> M;
+
+    cout << N << " + " << M << " = " << N + M << endl << endl;
+    cout << N << " - " << M << " = " << N - M << endl << endl;
+    cout << N << " * " << M << " = " << N * M << endl << endl;
+    cout << N << " / " << M << " = " << N / M << endl << endl;
+    cout << N << " % " << M << " = " << N % M << endl;
+
+    return 0;
+}
