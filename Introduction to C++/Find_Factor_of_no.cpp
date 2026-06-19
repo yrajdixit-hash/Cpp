c1d0dcf48b7b29417d2a0e0af7b1aadbb66e7c74
Yrajdixit <yrajdixit@gmail.com>
2026-06-19 21:46:32 +0530

    Add program to find factors of a number

---
+++ Introduction to C++/Find_Factor_of_no.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factors of " << n << " are: ";
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
