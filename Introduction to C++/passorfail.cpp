1425ce9d1279027212a1242c2ebbe64735546d8c
Yrajdixit <yrajdixit@gmail.com>
2026-06-22 20:40:12 +0530

    Add files via upload

---
+++ Introduction to C++/passorfail.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cin >> marks;
+
+    if (marks >= 35)
+        cout << "Pass";
+    else
+        cout << "Fail";
+
+    return 0;
+}
